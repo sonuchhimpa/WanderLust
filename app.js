1c1b5cebf8c6d40bbc11de1ccbb71764ce3fe737
Sonu Chhimpa <140334232+sonuchhimpa@users.noreply.github.com>
2025-06-27 11:56:26 +0530

    Structurize the code via comments

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-// Getting packages
+// ---------------------- Getting packages ---------------------->
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const app = express();
+const port = 8080;
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
 
-// Home Route API
+// ---------------------- Connection setup ---------------------->
+main().then(()=>{
+    console.log("Connected to Database......");
+})
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+
+// ------------------------- API ROUTE ------------------------->
+// Home Route
 app.get("/", (req,res)=>{
     res.send("Hi, i am root");
 });
 
 
 
-// Server Setup
-app.listen(8080,()=>{
-    console.log("App is listning on port: 8080");
+// ---------------------- Server Setup ---------------------->
+app.listen(port,()=>{
+    console.log(`App is listning on port: 8080 ${port}`);
 });
 
