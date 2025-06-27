21d1310e87d62c290e6e1783b595fccfb6fbd7ed
Sonu Chhimpa <140334232+sonuchhimpa@users.noreply.github.com>
2025-06-27 12:22:08 +0530

    Testing of Database

---
+++ Model/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const listningSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : String,
+    image : {
+        type : String,
+        default : "https://plus.unsplash.com/premium_photo-1685133855379-711aa008f7ba?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        set : (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1685133855379-711aa008f7ba?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price : Number,
+    locaton : String,
+    country : String
+});
+
+const listing = mongoose.model("listing", listningSchema);
+module.exports = listing;
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
-
+const listing = require("./Model/listing");
 
 // ---------------------- Connection setup ---------------------->
 main().then(()=>{
@@ -22,6 +22,19 @@ app.get("/", (req,res)=>{
     res.send("Hi, i am root");
 });
 
+// Test Route
+app.get("/testListing" , async (req,res)=>{
+    let sampleListing = new listing({
+        title : "My new Home",
+        description : "by the beach",
+        price : 1200,
+        locaton : "goa",
+        country : "India"
+    })
+    await sampleListing.save();
+    console.log("Sample is saved");
+    res.send("Successful testing");
+});
 
 
 // ---------------------- Server Setup ---------------------->
