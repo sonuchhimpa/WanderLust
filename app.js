be9aa94383bb597a8665995d3f02949631235cc6
Sonu Chhimpa <140334232+sonuchhimpa@users.noreply.github.com>
2025-06-29 13:28:19 +0530

    added Navbar

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 8080;
 const listing = require("./Model/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 // ---------------------- Connection setup ---------------------->
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
@@ -26,6 +27,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 // ------------------------- API ROUTE ------------------------->
 // Home Route
 app.get("/", (req, res) => {
