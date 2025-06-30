35716c7fff5db2fe1acb48587169fbfb9a094a0e
Sonu Chhimpa <140334232+sonuchhimpa@users.noreply.github.com>
2025-06-30 23:34:52 +0530

    edit page error

--- init/index.js
+++ init/index.js
@@ -5,23 +5,22 @@ const listing = require("../Model/listing.js");
 // ---------------------- Connection setup ---------------------->
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 main()
-    .then(()=>{
-        console.log("Connected to Database......");
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
+  .then(() => {
+    console.log("Connected to Database......");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(mongo_url);
+  await mongoose.connect(mongo_url);
 }
-
 
 // -------------------- Initialize Database -------------------->
-const initDB = async () =>{
-    await listing.deleteMany({});
-    await listing.insertMany(data.data);
-    console.log("Data is initialized");
-}
+const initDB = async () => {
+  await listing.deleteMany({});
+  await listing.insertMany(data.data);
+  console.log("Data is initialized");
+};
 
 initDB();
