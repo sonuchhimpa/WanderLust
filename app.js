2d74c67afeb78e563e6a7df5e68ded93dc2d8bf5
Sonu Chhimpa <140334232+sonuchhimpa@users.noreply.github.com>
2025-07-03 10:57:05 +0530

    Custom error page

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.delete(
 // ---------------------- Middlewares ---------------------->
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 // ---------------------- Server Setup ---------------------->
